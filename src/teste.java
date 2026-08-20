597a81a2d52f5f34cbbbbb1f7da6699a6f80ce34
Giovanni Rodrigues <giovannicorrearodrigues124@gmail.com>
2026-08-20 19:11:22 -0300

    Aprendendo Calendar

--- src/teste.java
+++ src/teste.java
@@ -1,7 +1,26 @@
-public class teste {
-    public static void main() {
-        System.out.println("Teste na branch");
-        System.out.println("Teste criado para uma branch");
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
 
+public class teste {
+    public static void main(String[] args)  throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Date d=Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+
+
+        System.out.println(sdf.format(d));
+
+        Calendar cal=Calendar.getInstance();
+        cal.setTime(d);
+        cal.add(Calendar.HOUR_OF_DAY,4);
+        int minutes=1+cal.get(Calendar.MINUTE);
+        int mount=1+cal.get(Calendar.MONTH);
+
+        System.out.println("minutes: "+minutes);
+
+        System.out.println("mount: "+mount);
     }
 }
