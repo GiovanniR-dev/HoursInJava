5014ad684634c304aa268722d1ea73ee0d5f68c0
Giovanni Rodrigues <giovannicorrearodrigues124@gmail.com>
2026-08-27 08:51:48 -0300

    exercicio de par e impar em for

--- src/teste.java
+++ src/teste.java
@@ -1,11 +1,29 @@
-import java.text.ParseException;
+import java.util.Scanner;
 
 public class teste {
-    public static void main(String[] args)  throws ParseException {
-        for (int i=1;i<=100; i++){
-            if(i% 2==1){
-                System.out.println(i);
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        int n1= sc.nextInt();
+
+        System.out.print("\nNumeros pares: ");
+
+        for (int i=1;i<=n1;i++){
+            if(i%2==0){
+                System.out.print(i+" ");
             }
         }
+
+        System.out.print("\nNúmeros ímpares:");
+
+        for(int i=1;i<=n1;i++){
+            if(i%2!=0){
+                System.out.print(i+" ");
+            }
+        }
+
+        sc.close();
+
     }
 }
