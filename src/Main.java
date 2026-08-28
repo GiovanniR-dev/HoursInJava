6aaf11a35d6f2af79857830238b3e5332aa94795
Giovanni Rodrigues <giovannicorrearodrigues124@gmail.com>
2026-08-28 08:16:49 -0300

    For

--- src/Main.java
+++ src/Main.java
@@ -8,26 +8,18 @@ import java.util.TimeZone;
 public class Main {
     public static void main(String[] args) throws ParseException {
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Digite o tamanho do lado do quadrado (1 a 20): ");
-        int tamanho = scanner.nextInt();
-
-        if (tamanho >= 1 && tamanho <= 20) {
-
-            for (int linha = 1; linha <= tamanho; linha++) {
-
-                for (int coluna = 1; coluna <= tamanho; coluna++) {
-                    System.out.print("*");
-                }
-
-                System.out.println();
-            }
-
-        } else {
-            System.out.println("Digite um tamanho entre 1 e 20.");
+        for(int i=0;i<=6;i++){
+            System.out.println("oi");
         }
 
-        scanner.close();
+        int resultado = 0;
+        int numero2 = 3;
+        int numero1 = 6;
+
+        for (int i = 0; i < numero2; i++) {
+            resultado += numero1;
+        }
+
+        System.out.println(resultado);
     }
 }
