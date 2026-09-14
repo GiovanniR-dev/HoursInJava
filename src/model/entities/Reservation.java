b4b101c26002e9af8f04a38a3b29a50eb9a7c770
Giovanni Rodrigues <giovannicorrearodrigues124@gmail.com>
2026-09-14 16:06:23 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,6 +1,5 @@
 package model.entities;
 
-import javax.naming.spi.ResolveResult;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -11,10 +10,7 @@ public class Reservation {
     private Date checkIn;
     private Date checkOut;
 
-    private static SimpleDateFormat sdf=new SimpleDateFormat("dd/mm/yyy");
-
-    public Reservation(){
-    }
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
         this.roomNumber = roomNumber;
@@ -38,28 +34,34 @@ public class Reservation {
         return checkOut;
     }
 
-    public long duration(){
+    public long duration() {
         long diff = checkOut.getTime() - checkIn.getTime();
-        return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDate(Date checkIn, Date checkOut){
-        this.checkIn=checkIn;
-        this.checkOut=checkOut;
+    public String updateDates(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            return "Reservation dates for update must be future dates";
+        }
+        if (!checkOut.after(checkIn)) {
+            return "Check-out date must be after check-in date";
+        }
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        return null;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "Room "
-                +roomNumber
-                +", Check-in: "
-                +sdf.format(checkIn)
-                +", Check-out"
-                +sdf.format(checkOut)
-                +", "
-                +duration()
-                +" nights";
+                + roomNumber
+                + ", check-in: "
+                + sdf.format(checkIn)
+                + ", check-out: "
+                + sdf.format(checkOut)
+                + ", "
+                + duration()
+                + " nights";
     }
-
-
 }
