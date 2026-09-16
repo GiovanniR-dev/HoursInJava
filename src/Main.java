845cc2e63073dfbddb5e4aa78a86e3e1c667cdba
Giovanni Rodrigues <giovannicorrearodrigues124@gmail.com>
2026-09-16 08:40:28 -0300

    Exercicio de fixacao do nelio alves:)

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,32 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
-
-import model.entities.Reservation;
-import model.exceptions.DomainException;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        try {
-            System.out.print("Room number: ");
-            int number = sc.nextInt();
-            System.out.print("Check-in date (dd/MM/yyyy): ");
-            Date checkIn = sdf.parse(sc.next());
-            System.out.print("Check-out date (dd/MM/yyyy): ");
-            Date checkOut = sdf.parse(sc.next());
-
-            Reservation reservation = new Reservation(number, checkIn, checkOut);
-            System.out.println("Reservation: " + reservation);
-
-            System.out.println();
-            System.out.println("Enter data to update the reservation:");
-            System.out.print("Check-in date (dd/MM/yyyy): ");
-            checkIn = sdf.parse(sc.next());
-            System.out.print("Check-out date (dd/MM/yyyy): ");
-            checkOut = sdf.parse(sc.next());
-
-            reservation.updateDates(checkIn, checkOut);
-            System.out.println("Reservation: " + reservation);
-        }
-        catch (ParseException e){
-            System.out.println("Invalid date format");
-        }
-        catch (DomainException e){
-            System.out.println("Error in reservation: "+ e.getMessage());
-        }
-        catch (RuntimeException e){
-            System.out.println("Unexpected error");
-        }
+        Scanner sc=new Scanner(System.in);
 
 
-        sc.close();
+
+        System.out.println("CONTA BANCARIA");
+        System.out.print("Nome do dono: ");
+        String nome=sc.nextLine();
+
+        System.out.print("Numero da conta: ");
+        int number=sc.nextInt();
+
+        System.out.print("Saldo: ");
+        double saldo=sc.nextDouble();
+
+        System.out.print("Limite de saque: ");
+        double limiteSaque=sc.nextDouble();
+
+        Account acc=new Account(number,nome,saldo,limiteSaque);
+        System.out.print("Valor para deposito: ");
+        acc.deposit(sc.nextDouble());
+
+        System.out.print("Valor para saque: ");
+        acc.WithDraw(sc.nextDouble());
+
+        System.out.print("Saldo final: "+acc.getBalance());
     }
 }
