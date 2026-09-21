93e2d038c420c6ddfbd798e703607f9aa49c9a05
Giovanni Rodrigues <giovannicorrearodrigues124@gmail.com>
2026-09-21 08:01:29 -0300

    Exercicio de file com scanner

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,28 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
 
+        File file=new File("C:\\Windows\\Temp");
+        Scanner sc=null;
 
+        try {
+            sc=new Scanner(file);
+            while (sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        finally {
+            if (sc !=null) {
+                sc.close();
+            }
+        }
 
-        System.out.println("CONTA BANCARIA");
-        System.out.print("Nome do dono: ");
-        String nome=sc.nextLine();
-
-        System.out.print("Numero da conta: ");
-        int number=sc.nextInt();
-
-        System.out.print("Saldo: ");
-        double saldo=sc.nextDouble();
-
-        System.out.print("Limite de saque: ");
-        double limiteSaque=sc.nextDouble();
-
-        Account acc=new Account(number,nome,saldo,limiteSaque);
-        System.out.print("Valor para deposito: ");
-        acc.deposit(sc.nextDouble());
-
-        System.out.print("Valor para saque: ");
-        acc.WithDraw(sc.nextDouble());
-
-        System.out.print("Saldo final: "+acc.getBalance());
     }
 }
