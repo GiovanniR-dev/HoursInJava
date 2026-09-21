958bf2ee589d930029f878ebe99862121faea112
Giovanni Rodrigues <giovannicorrearodrigues124@gmail.com>
2026-09-21 08:16:18 -0300

    Exercicio de filereader e bufferedreader

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -6,23 +8,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File file=new File("C:\\Windows\\Temp");
-        Scanner sc=null;
+        String path="c://windows//temp//in.txt";
+        FileReader fr=null;
+        BufferedReader br=null;
 
         try {
-            sc=new Scanner(file);
-            while (sc.hasNextLine()){
-                System.out.println(sc.nextLine());
+            fr=new FileReader(path);
+            br=new BufferedReader(fr);
+
+            String line=br.readLine();
+
+            while (line != null){
+                System.out.println(line);
+                line=br.readLine();
             }
         }
         catch (IOException e){
-            System.out.println("Error: "+e.getMessage());
+            System.out.println("Error : "+e.getMessage());
         }
         finally {
-            if (sc !=null) {
-                sc.close();
+            try {
+
+
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            }
+            catch (IOException e){
+                e.printStackTrace();
             }
         }
-
     }
 }
