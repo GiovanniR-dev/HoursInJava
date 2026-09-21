914e9692b8a37ebe2b2c05cc4adc93e328386166
Giovanni Rodrigues <giovannicorrearodrigues124@gmail.com>
2026-09-21 08:43:47 -0300

    Criacao de arquivo com file e buffered wrinter

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,22 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        String [] lines=new String[]{"Good morning", "Good afternoon", "Good night"};
 
-        String path="c://windows//temp//in.txt";
+        String path="c://windows//temp//out.txt";
 
-
-        try (BufferedReader br=new BufferedReader(new FileReader(path))){
-
-            String line=br.readLine();
-
-            while (line != null){
-                System.out.println(line);
-                line=br.readLine();
+        try(BufferedWriter bw=new BufferedWriter(new FileWriter(path))){
+            for (String line: lines){
+                bw.write(line);
+                bw.newLine();
             }
         }
         catch (IOException e){
-            System.out.println("Error : "+e.getMessage());
+            e.printStackTrace();
         }
     }
 }
